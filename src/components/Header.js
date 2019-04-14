b66df3016cf71f89a01e87959cc3dfb67e23a322
Magdalena Semik <mgd.kaczmarek@gmail.com>
2019-04-14 12:05:31 +0200

    make cr fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ import pistolSrc from '../assets/img/pistol.png';
 const Header = () => (
     <div className="header">
         <h1 className="header__title">SHOOT GAME</h1>
-        <img className="pistolImg pistolImg--right" alt="Gun" src={pistolSrc} />
-        <img className="pistolImg pistolImg--left" alt="Gun" src={pistolSrc} />
+        <img className="pistol pistol--right" alt="Gun" src={pistolSrc} />
+        <img className="pistol pistol--left" alt="Gun" src={pistolSrc} />
     </div>
 );
 
